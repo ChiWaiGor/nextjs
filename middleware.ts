ebf71f00247678a96aee8584b0c06bd84266ef92
Chester Zhang <chaz.cheung@gmail.com>
2026-03-17 23:22:59 +1100

    finished tutorial

---
+++ middleware.ts
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth";
+import { authConfig } from "./auth.config";
+
+export default NextAuth(authConfig).auth;
+
+export const config = {
+  // https://nextjs.org/docs/app/api-reference/file-conventions/proxy#matcher
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+};
+
+// export const config = {
+//   matcher: ["/:path*"],
+// };
